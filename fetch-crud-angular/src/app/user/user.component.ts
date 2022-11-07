9b081ab4b4406dceef9ba7688d7218d1a9983569
Luigi Indaco <luigindaco@gmail.com>
2022-11-07 22:29:49 +0100

    commit

---
+++ fetch-crud-angular/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent implements OnInit {
+  userList: User[] = [];
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllUser().subscribe((resp) => (this.userList = resp));
+  }
+  removeUser(user: User): void {
+    this.userService.removeUser(user.id).subscribe((resp) => {
+      this.userList = this.userList.filter((elem) => elem.id !== user.id);
+    });
+  }
+}
